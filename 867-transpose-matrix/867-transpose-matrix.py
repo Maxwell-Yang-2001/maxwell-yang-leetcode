f477f65ad27b02572809cc73b9e965981e249d02
Maxwell Yang <migacanada@gmail.com>
2022-06-01 20:23:13 -0700

    Time: 101 ms (47.42%), Space: 14.9 MB (6.50%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,6 +1,13 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         width, height = len(matrix[0]), len(matrix)
+        # square matrix - could be optimized
+        if width == height:
+            for i in range(width):
+                for j in range(i + 1, width):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            return matrix
+        
         result = [[0] * height for i in range(width)]
         
         for i in range(height):
